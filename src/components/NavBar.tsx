e61f0484706dd624e322c3c21b6dc707322a204f
Andrew Wong <kkwong125@gmail.com>
2021-10-12 12:28:19 -0700

    Fixed blinked navbar links when refresh

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,41 +7,51 @@ import { Button } from "@chakra-ui/button";
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-  const [{ fetching }, logout] = useLogoutMutation();
-  const [{ data }, me] = useMeQuery({
+  const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
+  const [{ data, fetching: meFetching }, me] = useMeQuery({
     requestPolicy: "cache-and-network",
   });
 
+  let links;
+
+  if (meFetching) {
+    // Show nothing when fetching user data
+  } else if (data?.me) {
+    // User logged in
+    links = (
+      <Flex>
+        <Box mr={5}>{data.me.username}</Box>
+        <Button
+          variant={"link"}
+          isLoading={logoutFetching}
+          onClick={async () => {
+            await logout();
+            me();
+          }}
+        >
+          Log Out
+        </Button>
+      </Flex>
+    );
+  } else {
+    // User not logged in
+    links = (
+      <>
+        <NextLink href="/login">
+          <Link color={"white"} mr={"5"}>
+            Log In
+          </Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link color={"white"}>Sign Up</Link>
+        </NextLink>
+      </>
+    );
+  }
+
   return (
     <Flex bg="darkgray" p={"4"}>
-      <Box ml={"auto"}>
-        {data?.me ? (
-          <Flex>
-            <Box mr={5}>{data.me.username}</Box>
-            <Button
-              variant={"link"}
-              isLoading={fetching}
-              onClick={async () => {
-                await logout();
-                me();
-              }}
-            >
-              Log Out
-            </Button>
-          </Flex>
-        ) : (
-          <>
-            <NextLink href="/login">
-              <Link color={"white"} mr={"5"}>
-                Log In
-              </Link>
-            </NextLink>
-            <NextLink href="/register">
-              <Link color={"white"}>Sign Up</Link>
-            </NextLink>
-          </>
-        )}
-      </Box>
+      <Box ml={"auto"}>{links}</Box>
     </Flex>
   );
 };
