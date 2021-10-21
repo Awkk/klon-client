a2067d38fb7ab34d3067a6dc7e46bf258c38a2d3
Andrew Wong <kkwong125@gmail.com>
2021-10-21 14:35:56 -0700

    Implemented cache update after vote on post

--- src/config/urqlClientConfig.ts
+++ src/config/urqlClientConfig.ts
@@ -1,13 +1,15 @@
 import {
-  cacheExchange,
   ClientOptions,
   CombinedError,
   dedupExchange,
   errorExchange,
   fetchExchange,
+  gql,
 } from "@urql/core";
 import { NextUrqlClientConfig, SSRExchange } from "next-urql";
 import Router from "next/router";
+import { cacheExchange } from "@urql/exchange-graphcache";
+import { VoteMutationVariables } from "../generated/graphql";
 
 const onError = (error: CombinedError) => {
   if (error.message.includes("not authenticated")) {
@@ -24,7 +26,31 @@ export const urqlClientConfig: NextUrqlClientConfig = (
   },
   exchanges: [
     dedupExchange,
-    cacheExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          vote(_result, args, cache) {
+            const { postId, value } = args as VoteMutationVariables;
+            const voteInfo = gql`
+              fragment _ on Post {
+                score
+                voteStatus
+              }
+            `;
+            const data = cache.readFragment(voteInfo, { id: postId });
+
+            if (data.voteStatus !== value) {
+              const newScore = data.score + value - data.voteStatus;
+              cache.writeFragment(voteInfo, {
+                id: postId,
+                score: newScore,
+                voteStatus: value,
+              });
+            }
+          },
+        },
+      },
+    }),
     errorExchange({
       onError,
     }),
