49d6fe7fc579293f57b41687b9cfd1a5e2314695
Andrew Wong <kkwong125@gmail.com>
2021-11-09 16:49:06 -0800

    Implemented favicon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import { useColorModeValue } from "@chakra-ui/color-mode";
 import { Box, Flex } from "@chakra-ui/layout";
+import Head from "next/head";
 import React from "react";
 import { NavBar } from "./NavBar";
 
@@ -9,11 +10,33 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
   const bgColor = useColorModeValue("gray.200", "gray.900");
 
   return (
-    <Box minH="100vh" bgColor={bgColor}>
-      <NavBar />
-      <Flex justifyContent="center" p="4" flex="1">
-        {children}
-      </Flex>
-    </Box>
+    <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <Box minH="100vh" bgColor={bgColor}>
+        <NavBar />
+        <Flex justifyContent="center" p="4" flex="1">
+          {children}
+        </Flex>
+      </Box>
+    </>
   );
 };
